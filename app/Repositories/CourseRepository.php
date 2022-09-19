81d1b9d648f5e4046ce960a362d3495f4040d602
Dimas Neto <dimas_fcn@outlook.com>
2022-09-19 15:28:58 -0300

    services

---
+++ app/Repositories/CourseRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Course;
+
+class CourseRepository
+{
+    protected $entity;
+
+    public function __contruct(Course $course)
+    {
+        $this->entity = $course;
+    }
+
+    public function getAllCourses()
+    {
+        return $this->entity->get();
+    }
+
+    public function createNewCourse(array $data)
+    {
+        return $this->entity->create($data);
+    }
+}
---
+++ app/Services/CourseService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\CourseRepository;
+
+class CourseService
+{
+    protected $repository;
+
+    public function __contruct(CourseRepository $courseRepository)
+    {
+        $this->repository = $courseRepository;
+    }
+
+    public function getCourses()
+    {
+        return $this->repository->getAllCourses();
+    }
+
+    public function createNewCourse(array $data)
+    {
+        return $this->repository->createNewCourse($data);
+    }
+}
